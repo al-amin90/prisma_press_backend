a8eac917ac39576a813056b0ddb9d5b858208af5
al-amin90 <ijesun30@gmail.com>
2026-07-07 17:43:32 +0600

    feat: Handling Prisma Errors With Global Error Handler

--- src/app/middlewares/GlobalErrorHandler.ts
+++ src/app/middlewares/GlobalErrorHandler.ts
@@ -5,6 +5,8 @@ import type {
   Response,
 } from "express";
 import AppError from "../utils/AppError";
+import { Prisma } from "../../../generated/prisma/client";
+import status from "http-status";
 
 const GlobalErrorHandler: ErrorRequestHandler = (
   err,
@@ -14,25 +16,39 @@ const GlobalErrorHandler: ErrorRequestHandler = (
 ) => {
   let statusCode = 500;
   let message = "Something Went Wrong!";
-  let errorSources;
 
-  if (err instanceof AppError) {
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    statusCode = 400;
+    message = "You have Provided incorrect field type or missing fields";
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      statusCode = 400;
+      message = "Duplicate Key Error";
+    } else if (err.code === "P2003") {
+      statusCode = 400;
+      message = "Foreign key constraint failed";
+    } else if (err.code === "2025") {
+      statusCode = 400;
+      message =
+        "An operation failed because it depends on one or more records that ware required but not found";
+    }
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
+    if (err.errorCode === "P1000") {
+      statusCode = status.UNAUTHORIZED;
+      message =
+        "Authentication failed against database server. Please Check Your Credentials";
+    } else if (err.errorCode === "P1001") {
+      statusCode = status.BAD_REQUEST;
+      message = "Can't reach database server";
+    }
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    statusCode = status.INTERNAL_SERVER_ERROR;
+    message = "Error Occured during query execution";
+  } else if (err instanceof AppError) {
     statusCode = err?.statusCode;
     message = err.message;
-    errorSources = [
-      {
-        path: "",
-        message: err.message,
-      },
-    ];
   } else if (err instanceof Error) {
     message = err.message;
-    errorSources = [
-      {
-        path: "",
-        message: err.message,
-      },
-    ];
   }
 
   console.log(":) Global bro", err);
@@ -40,7 +56,6 @@ const GlobalErrorHandler: ErrorRequestHandler = (
   res.status(statusCode).json({
     success: false,
     message,
-    errorSources,
     // err,
     stack: err.stack,
   });
